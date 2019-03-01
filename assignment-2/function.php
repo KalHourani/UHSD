50b2ecba99bf598c42fc57c2bc8c0e90c8e5b9a0
aegilgut <31905464+aegilgut@users.noreply.github.com>
2019-03-01 00:13:23 -0600

    Footer file and Function file
    Footer file uploaded, mostly empty, there for completion. Function file uploaded,s tandard registry function and checks for invalid characters, empty fields, etc.

---
+++ assignment-2/function.php
@@ -0,0 +1,89 @@
+<?php 
+
+    // register function
+    function RegisterFunc()
+    {
+            $Message= "";
+
+            if(isset($_GET['Empty']))
+                {
+                    $Message = "Please fill in the blanks"; 
+                    echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+                }
+
+            if(isset($_GET['characters']))
+                {
+                    $Message = "Please enter valid characters"; 
+                    echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+                }
+
+            if(isset($_GET['ValidEmail']))
+                {
+                    $Message = "Please enter a valid email address"; 
+                    echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+                }
+
+            if(isset($_GET['User Taken']))
+                {
+                    $Message = "Username is already taken"; 
+                    echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+                }
+
+            if(isset($_GET['Email Taken']))
+                {
+                    $Message = "Email is already taken"; 
+                    echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+                }
+
+            if(isset($_GET['Invalid_Format']))
+                {
+                    $Message = "Invalid image format"; 
+                    echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+                }
+
+            if(isset($_GET['Too_Large']))
+                {
+                    $Message = "Image too large"; 
+                    echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+                }
+
+            if(isset($_GET['Success']))
+                {
+                    $Message = "You have successfully made an account!"; 
+                    echo '<div class="alert alert-success text-center">'.$Message.'
+                    <a href="login.php">Login Now</a>
+                    </div>';
+                }
+    }
+
+    // login function
+    function LoginFunc()
+    {
+        $Message = "";
+
+        if(isset($_GET['empty']))
+        {
+            $Message = "Please fill in the blanks!"; 
+            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+        }
+
+        if(isset($_GET['invalid']))
+        {
+            $Message = "Username does not match"; 
+            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+        }
+
+        if(isset($_GET['password_invalid']))
+        {
+            $Message = "Password does not match"; 
+            echo '<div class="alert alert-danger text-center">'.$Message.'</div>';
+        }
+
+        if(isset($_SESSION['EmployeeID']))
+        {
+            header('location:index.php');
+        }
+
+    }
+
+?>
